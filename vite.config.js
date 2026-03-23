cda80cfd97f51fc5655efa1fc209d58630788150
indexEV <index.error404@gmail.com>
2026-03-23 15:35:21 +0000

    fix vite.config.js merge conflict

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/EVolution/',
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/EVolution/',
->>>>>>> c8f72e86189094634083d6ae60238dbc986c0414
 })
