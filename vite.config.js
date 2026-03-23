5979a75e8509a8767cdf01d2e57eb7fb8c88b05c
indexEV <index.error404@gmail.com>
2026-03-23 16:03:38 +0000

    Fix files for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/EVolution/',
+  base: '/EVolution/', // matches your GitHub repo folder
 })
